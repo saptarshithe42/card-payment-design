126fb91bb1172f62e6eaa1eb01ef7abb353031ba
Saptarshi Mondal <saptarshi.the.42@gmail.com>
2022-11-05 23:04:51 +0530

    card icon toggle implemented

--- index.js
+++ index.js
@@ -25,6 +25,21 @@ for(let i = 0; i < numberOfCardInputs; i++)
     });
 }
 
+const card_logo_names = ["mastercard", "visa", "paypal", "discover", "stripe", "diners-club", "amazon-pay"];
+
+let logo_index = 0;
+var len = card_logo_names.length;
+
+var card_icon = document.querySelector(".card-icon-span");
+
+
+card_icon.addEventListener("click", function(){
+
+    let new_html = "<i class=\"fa-brands fa-cc-"+ card_logo_names[logo_index % len] + " fa-4x card-icon\"></i>";
+    logo_index += 1;
+    card_icon.innerHTML = new_html;
+});
+
 
 /*
 card_holder_name_input.addEventListener("input", function(event){
